617910a72d215c8d685a6d0d2a6c7fd37455cef8
chhaichivon <chhaichivon1995@gmail.com>
2017-10-29 03:47:28 +0700

    genereate post , category and auth

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
 	return view('admin.index');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
